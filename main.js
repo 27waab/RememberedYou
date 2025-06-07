6f54b1b7cea7673a3763eb18b36da6a51ab34117
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-06-07 22:52:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const addBtn = document.getElementById("addBtn"), items = document.querySelector(".items"), ovarlay = document.querySelector(".overlay");
+const newQuitBtn = document.getElementById("newQuit"), content = document.getElementById("sectionOfContent");
+const newQuit = document.getElementById("newQuit"), btn_close = document.querySelector(".btn-close"), module = document.querySelector(".moduleOfNewItems");
+let arr = [];
 
 addBtn.onclick = () => {
     addBtn.classList.toggle("open");
     items.classList.toggle("open");
+    ovarlay.classList.toggle("open");
+}
+newQuit.onclick = () => {
+    addBtn.classList.toggle("open");
+    module.classList.toggle("open");
+    items.classList.toggle("open");
+}
+btn_close.onclick = () => {
+    module.classList.toggle("open");
     ovarlay.classList.toggle("open");
 }
